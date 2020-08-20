62cfb86f5de8675db7e086db9b86783d7ac948da
mangohero <mangohero1985@hotmail.com>
2020-08-20 23:25:11 +0900

    update udf by annotation

--- utils.py
+++ utils.py
@@ -1,15 +1,13 @@
 import numpy as np
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
+from pyspark.sql.functions import udf
 
-
+@udf(T.FloatType())
 def cosine_similarity(a, b):
     return float(np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b)))
 
-
-cosine_similarity_udf = F.udf(cosine_similarity, T.FloatType())
-
-
 def compute_similarity(df):
-    rst = df.withColumn("c", cosine_similarity_udf(F.col("a"), F.col("b"))).select("c")
+    rst = df.withColumn("c", cosine_similarity(F.col("a"), F.col("b"))).select("c")
     return rst
+
